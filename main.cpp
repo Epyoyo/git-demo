ea53d666f8a05ad3f0ab4cf90575dcde8969c758
Epyoyo <1171387786@qq.com>
2025-01-26 18:59:49 +0800

    third save

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
 
+#include<Windows.h>
+
 int main()
 {
     printf("hello git\n");
     printf("hello vs code\n");
+   
+    system("pause");
     return 0;
 }
